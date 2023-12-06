04fe8e7051688578aa0d3744891e91aa8a7d5ff5
smukvica <vendramin.mitja@gmail.com>
2023-12-06 15:56:44 +0100

    day 6 part 2; changed function to uint64_t cuz part 2

--- src/day_6.c
+++ src/day_6.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int can_beat(int a_time, int a_distance, int a_hold){
-    int time_left = a_time - a_hold;
+uint64_t can_beat(uint64_t a_time, uint64_t a_distance, uint64_t a_hold){
+    uint64_t time_left = a_time - a_hold;
     if(time_left * a_hold >= a_distance)
         return 1;
     return 0;
+}
+
+uint64_t append_numbers_together(int a_array[4]){
+
+    uint64_t temps[4];
+    for(int i = 0; i < 4; i++){
+        temps[i] = (uint64_t)a_array[i];
+    }
+
+
+    for(int i = 3; i > 0; i--){
+        int p = 1;
+        int t = a_array[i];
+        while(t > 0){
+            t /= 10;
+            p *= 10;
+        }
+        for(int j = 0; j < i; j++){
+            temps[j] *= p;
+        }
+    }
+    uint64_t n = 0;
+    for(int i = 0; i < 4; i++){
+        n += temps[i];
+    }
+    return n;
 }
 
 int main(){
@@ -17,6 +44,7 @@ int main(){
 
     scanf("Distance: %d %d %d %d\n", &distances[0], &distances[1], &distances[2], &distances[3]);
 
+    
 
     for(int i = 0; i < 4; i++){
         int a = 0;
@@ -38,9 +66,28 @@ int main(){
         ways[i] = b - a + 1;
     }
 
-    
+    printf("ways1: %d\n", ways[0] * ways[1] * ways[2] * ways[3]);
 
-    printf("ways: %d\n", ways[0] * ways[1] * ways[2] * ways[3]);
+    uint64_t time_k = append_numbers_together(times);
+    uint64_t distance_k = append_numbers_together(distances);
+
+    int a = 0;
+    int b = 0;
+    for(int j = 1; j < time_k; j++){
+        if(can_beat(time_k, distance_k, j) == 1){
+            a = j;
+            break;
+        }
+    }
+
+    for(int j = time_k; j > 0; j--){
+        if(can_beat(time_k, distance_k, j) == 1){
+            b = j;
+            break;
+        }
+    }
+
+    printf("ways2: %lu\n", b - a + 1);
 
     return 0;
 }
